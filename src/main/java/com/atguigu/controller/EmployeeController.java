b521be01b31c24e8876c3099f5c2f84341231c00
tiancailyb <2224615397@qq.com>
2020-03-29 19:56:42 +0800

    Springboot02QuickApplication

--- src/main/java/com/atguigu/controller/EmployeeController.java
+++ src/main/java/com/atguigu/controller/EmployeeController.java
@@ -4,12 +4,11 @@ import com.atguigu.dao.DepartmentDao;
 import com.atguigu.dao.EmployeeDao;
 import com.atguigu.entities.Department;
 import com.atguigu.entities.Employee;
+import com.sun.org.glassfish.gmbal.ParameterNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Map;
@@ -42,7 +41,15 @@ public class EmployeeController {
 
         System.out.println(employee);
         employeeDao.save(employee);
-
         return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String updateEmp(@PathVariable("id") Integer id, Model model){
+        Employee employee = employeeDao.get(id);
+        Department department = employee.getDepartment();
+        model.addAttribute("depts", department);
+        model.addAttribute("emp", employee);
+        return "emp/add";
     }
 }
